b04616cff65c3c2aa24029b8bae3b2a7a5b0125a
ada mancini <adab3ta@jbanetwork.com>
2017-01-10 10:43:18 -0500

    Manage netdata service

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,2 +1,17 @@
 class netdata::service inherits netdata {
+
+  if ! ($netdata::service_ensure in [ 'running', 'stopped' ]) {
+    fail('service_ensure parameter must be running or stopped')
+  }
+
+  if $netdata::service_manage == true {
+    service { 'netdata':
+      ensure     => $netdata::service_ensure,
+      enable     => $netdata::service_enable,
+      name       => $netdata::service_name,
+      provider   => $netdata::service_provider,
+      hasstatus  => true,
+      hasrestart => true,
+    }
+  }
 }
