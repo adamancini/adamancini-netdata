5a19c16d6c97e124bed20738b7f42a36849ab6b5
ada mancini <adab3ta@jbanetwork.com>
2017-01-10 13:39:52 -0500

    Make sure the netdata config file exists, owned by netdata

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,20 @@
 class netdata::config inherits netdata {
 
   $conf_dir = '/etc/netdata'
+  $conf_file = "${conf_dir}/netdata.conf"
   # stub
   file { $conf_dir:
     ensure => directory,
+    owner  => 'netdata',
+    group  => 'netdata',
+    mode   => '0755',
+    before => $conf_file,
+  }
+
+  file { $conf_file:
+    ensure => present,
+    owner  => 'netdata',
+    group  => 'netdata',
+    mode   => '0664',
   }
 }
