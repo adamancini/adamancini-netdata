3bfe1d0f046d50766625445bea6ce8b326b0f868
ada mancini <adab3ta@jbanetwork.com>
2017-01-10 17:21:59 -0500

    Use top-level variables set in common.yaml as paths to file resources

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,28 +1,26 @@
 class netdata::config inherits netdata {
 
-  $config_directory = $netdata::config_dir
-
   if $netdata::config_manage {
 
     notify { 'update netdata configuration': }
 
-    file { $config_directory:
+    file { $netdata::config_dir:
       ensure => directory,
       owner  => $netdata::service_name,
       group  => $netdata::service_name,
       mode   => '0755',
     }
 
-    file { "${config_directory}/netdata.conf":
+    file { $netdata::config_file:
       ensure  => present,
       owner   => $netdata::service_name,
       group   => $netdata::service_name,
       mode    => '0664',
-      require => File[$config_directory],
+      require => File[$netdata::config_dir],
     }
 
     validate_hash($netdata::config::options)
-    $ini_defaults = { 'path' => "${netdata::config_dir}/netdata.conf" }
+    $ini_defaults = { 'path' => $netdata::config_file }
     create_ini_settings($netdata::config::options, $ini_defaults)
   }
 }
