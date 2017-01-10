f00f3783bfb8e7000fc2ab4dd5ae8e0c9cc3ab45
ada mancini <adab3ta@jbanetwork.com>
2017-01-10 17:39:54 -0500

    set defaults for Ini_setting

--- manifests/config.pp
+++ manifests/config.pp
@@ -20,7 +20,10 @@ class netdata::config inherits netdata {
     }
 
     validate_hash($netdata::options)
-    $ini_defaults = { 'path' => $netdata::config_file }
+    $ini_defaults = {
+      'path' => $netdata::config_file,
+      'ensure' => 'present',
+    }
     create_ini_settings($netdata::options, $ini_defaults)
   }
 }
