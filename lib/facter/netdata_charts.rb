5c58d5d66bc4476a0e5f1d96c5f7462ced35d544
ada mancini <adab3ta@jbanetwork.com>
2017-01-19 18:00:23 -0500

    Use Facter::Util::Resolution instead of the other thing

--- lib/facter/netdata_charts.rb
+++ lib/facter/netdata_charts.rb
@@ -2,7 +2,7 @@ require 'json'
 Facter.add(:netdata_charts) do
   setcode do
     charts_hash = {}
-    charts_hash = Facter::Core::Execution.exec('curl -X GET http://localhost:19999/api/v1/charts')
-    puts JSON.pretty_generate(charts_hash, opts = nil)
+    charts_hash = Facter::Util::Resolution.exec('/usr/bin/curl -X GET http://localhost:19999/api/v1/charts')
+    JSON.pretty_generate(charts_hash, opts = nil)
   end
 end
