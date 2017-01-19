aa98b20d3771259b0c068776e17b6b63772b54f6
ada mancini <adab3ta@jbanetwork.com>
2017-01-19 18:39:37 -0500

    Clean up fact code

--- lib/facter/netdata_charts.rb
+++ lib/facter/netdata_charts.rb
@@ -1,9 +1,7 @@
 require 'json'
 Facter.add(:netdata_charts) do
   setcode do
-    charts_hash = {}
     charts_json = Facter::Core::Execution.exec('/usr/bin/curl -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
     charts_hash = JSON.parse(charts_json)
-    charts_hash
   end
 end
