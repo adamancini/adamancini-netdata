041be1ab1e3157ca566fded0cd6fd48be3ac15c0
ada mancini <adab3ta@jbanetwork.com>
2017-01-19 18:08:51 -0500

    Trim all whitespace

--- lib/facter/netdata_charts.rb
+++ lib/facter/netdata_charts.rb
@@ -2,7 +2,7 @@ require 'json'
 Facter.add(:netdata_charts) do
   setcode do
     charts_hash = {}
-    charts_hash = Facter::Util::Resolution.exec('/usr/bin/curl -X GET http://localhost:19999/api/v1/charts')
+    charts_hash = Facter::Util::Resolution.exec('/usr/bin/curl -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
     JSON.pretty_generate(charts_hash, opts = nil)
   end
 end
