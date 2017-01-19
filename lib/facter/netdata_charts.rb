63bf75e39555e38be9e7167f8e91ec88dc21e01a
ada mancini <adab3ta@jbanetwork.com>
2017-01-19 17:56:17 -0500

    Pretty-print the json results

--- lib/facter/netdata_charts.rb
+++ lib/facter/netdata_charts.rb
@@ -1,5 +1,8 @@
+require 'json'
 Facter.add(:netdata_charts) do
   setcode do
-    Facter::Core::Execution.exec('curl -X GET http://localhost:19999/api/v1/charts')
+    charts_hash = {}
+    charts_hash = Facter::Core::Execution.exec('curl -X GET http://localhost:19999/api/v1/charts')
+    puts JSON.pretty_generate(charts_hash, opts = nil)
   end
 end
