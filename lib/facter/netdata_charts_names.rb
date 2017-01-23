26916ad7cfa50ebeb32d64cb35c7af4c1d5ea37e
ada mancini <adab3ta@jbanetwork.com>
2017-01-23 17:49:53 -0500

    Enable netdata_charts_names and remove debugging stub

--- lib/facter/netdata_charts_names.rb
+++ lib/facter/netdata_charts_names.rb
@@ -2,9 +2,8 @@ require 'json'
 Facter.add(:netdata_charts_names) do
   setcode do
     if File.exists? '/etc/netdata'
-      # charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\' | jq \'.charts[].id\'')
-      # charts_hash = JSON.parse(charts_json)
-      charts_hash = {}
+      charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\' | jq \'. | { hostname: .hostname, charts: [.charts[] | .id] }\'')
+      charts_hash = JSON.parse(charts_json)
     end
   end
 end
