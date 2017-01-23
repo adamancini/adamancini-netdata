5e94708bfe1b894835faee53d03bf22fcfaf46bd
ada mancini <adab3ta@jbanetwork.com>
2017-01-23 15:58:45 -0500

    Make this fact a no-op while i test another method

--- lib/facter/netdata_charts.rb
+++ lib/facter/netdata_charts.rb
@@ -1,7 +1,9 @@
 require 'json'
 Facter.add(:netdata_charts) do
   setcode do
-    charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
-    charts_hash = JSON.parse(charts_json)
+    if File.exists? '/etc/netdata'
+      charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
+      charts_hash = JSON.parse(charts_json)
+    end
   end
 end
