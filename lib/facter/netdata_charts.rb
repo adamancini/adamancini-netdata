228ce35e3f06f8c1bdf6d5e7e7ee4a31cc326162
ada mancini <adab3ta@jbanetwork.com>
2017-01-23 15:46:27 -0500

    trim whitespace from netdata_charts_names before pipe to jq

--- lib/facter/netdata_charts.rb
+++ lib/facter/netdata_charts.rb
@@ -1,7 +1,7 @@
 require 'json'
 Facter.add(:netdata_charts) do
   setcode do
-    charts_json = Facter::Core::Execution.exec('/usr/bin/curl -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
+    charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
     charts_hash = JSON.parse(charts_json)
   end
 end
--- lib/facter/netdata_charts_names.rb
+++ lib/facter/netdata_charts_names.rb
@@ -1,7 +1,7 @@
 require 'json'
 Facter.add(:netdata_charts_names) do
   setcode do
-    charts_json = Facter::Core::Execution.exec('curl -X GET localhost:19999/api/v1/charts | jq \'.charts[].id\'')
+    charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\' | jq \'.charts[].id\'')
     charts_hash = JSON.parse(charts_json)
   end
 end
