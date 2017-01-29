845ed973c4211ddbe19c8380591918bb1cd5a050
ada mancini <adab3ta@jbanetwork.com>
2017-01-29 07:02:40 -0500

    Undo

--- manifests/install.pp
+++ manifests/install.pp
@@ -54,17 +54,25 @@ class netdata::install inherits netdata {
 
   case $::facts['service_provider'] {
     'upstart': {
+      exec { 'Install init.d/upstart compatible service file':
+        command => "cp /opt/netdata/${netdata::service_file} ./netdata",
+        cwd     => '/etc/init.d/',
+        creates => '/etc/init.d/netdata',
+        before  => File['/etc/init.d/netdata'],
+      }
+
       file { '/etc/init.d/netdata':
-        ensure  => link,
-        target  => '/opt/netdata/system/netdata-lsb',
-        require => Vcsrepo['/opt/netdata']
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
       }
     }
     'systemd': {
-      notify { 'Systemd service file installed automatically': }
+      notify {"Systemd service file installed automatically": }
     }
     default: {
-      notify { 'Default is a noop': }
+      notify {"Default is a noop": }
     }
   }
 
