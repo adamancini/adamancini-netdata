759979d7dd9d8e10deb1a66513642a7b398d66a4
ada mancini <adab3ta@gmail.com>
2017-02-08 16:34:21 -0500

    Turn off sending all chart data - only send the chart names

--- lib/facter/netdata_charts.rb
+++ lib/facter/netdata_charts.rb
@@ -2,8 +2,8 @@ require 'json'
 Facter.add(:netdata_charts) do
   setcode do
     if File.exists? '/etc/netdata'
-      charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
-      charts_hash = JSON.parse(charts_json)
+      # charts_json = Facter::Core::Execution.exec('/usr/bin/curl --silent -X GET http://localhost:19999/api/v1/charts | tr -d \'\\040\\011\\012\\015\'')
+      # charts_hash = JSON.parse(charts_json)
     end
   end
 end
