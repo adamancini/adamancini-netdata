68097161b94070489db878d3302f5446a72beae5
ada mancini <adab3ta@gmail.com>
2017-02-24 03:54:20 -0500

    alarms doesn't depend on config also being managed

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,22 +10,22 @@ class netdata::config inherits netdata {
       mode   => '0664',
     }
 
-    if $netdata::alarms_manage == true {
-      file { 'netdata alarms notifications':
-        ensure  => present,
-        content => template('netdata/health_alarm_notify.conf.erb'),
-        path    => $netdata::health_alarms_notify_config,
-        owner   => $netdata::service_name,
-        group   => $netdata::service_name,
-        mode    => '0755',
-      }
-    }
-
     validate_hash($netdata::options)
     $ini_defaults = {
       'path' => $netdata::config_file,
       'ensure' => 'present',
     }
     create_ini_settings($netdata::options, $ini_defaults)
+  }
+
+  if $netdata::alarms_manage == true {
+    file { 'netdata alarms notifications':
+      ensure  => present,
+      content => template('netdata/health_alarm_notify.conf.erb'),
+      path    => $netdata::health_alarms_notify_config,
+      owner   => $netdata::service_name,
+      group   => $netdata::service_name,
+      mode    => '0755',
+    }
   }
 }
