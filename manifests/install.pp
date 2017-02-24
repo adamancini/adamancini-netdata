fe777a8a3dcc969251db9698f431ccc3d4d7df42
ada mancini <adab3ta@gmail.com>
2017-02-24 00:08:11 -0500

    Install from git OR from SemVer release tarball

--- manifests/install.pp
+++ manifests/install.pp
@@ -40,17 +40,39 @@ class netdata::install inherits netdata {
     ensure_packages( 'jq', {'ensure' => 'present'} )
   }
 
-  vcsrepo { $netdata::repo_location:
-    ensure   => $netdata::repo_ensure,
-    provider => git,
-    source   => $netdata::installation_source,
+  if $netdata::install_from_git == true {
+
+    vcsrepo { $netdata::repo_location:
+      ensure   => $netdata::repo_ensure,
+      provider => git,
+      source   => $netdata::installation_source,
+      before   => Exec['Install netdata'],
+    }
+
+    if $netdata::update_with_cron == true {
+      cron { 'netdata-updater.sh':
+        command => "${netdata::repo_location}/netdata-updater.sh",
+        hour    => $netdata::update_cron_hour,
+        minute  => $netdata::update_cron_min,
+        user    => $netdata::update_cron_user,
+      }
+    }
+  }
+  else {
+
+    $installation_source = "https://github.com/firehol/netdata/releases/download/v${netdata::release_version}/netdata-${netdata::release_version}.tar.gz"
+
+    exec { "Download netdata-${netdata::release_version}":
+      command => "/usr/bin/wget -qO- ${installation_source} | tar xvz -C /root/",
+      creates => "/root/netdata-${netdata::release_version}",
+      before  => Exec['Install netdata']
+    }
   }
 
-  exec { 'Install_netdata':
+  exec { 'Install netdata':
     command     => "${netdata::repo_location}/netdata-installer.sh",
     cwd         => $netdata::repo_location,
     creates     => $netdata::config_dir,
-    subscribe   => Vcsrepo[$netdata::repo_location],
     refreshonly => true,
   }
 }
