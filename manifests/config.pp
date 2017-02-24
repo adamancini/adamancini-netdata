d6347a4f12f62e4a683377de98c383c5098b23ae
ada mancini <adab3ta@gmail.com>
2017-02-24 00:07:50 -0500

    Support configuring health_alarm_notifications.conf

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,13 +1,24 @@
 class netdata::config inherits netdata {
 
   if $netdata::config_manage {
-    
+
     file { 'netdata config file':
-      ensure => file,
+      ensure => present,
       path   => $netdata::config_file,
       owner  => $netdata::service_name,
       group  => $netdata::service_name,
       mode   => '0664',
+    }
+
+    if $netdata::alarms_manage == true {
+      file { 'netdata alarms notifications':
+        ensure  => present,
+        content => template('netdata/health_alarm_notify.conf.erb'),
+        path    => $netdata::health_alarms_notify_config,
+        owner   => $netdata::service_name,
+        group   => $netdata::service_name,
+        mode    => '0755',
+      }
     }
 
     validate_hash($netdata::options)
